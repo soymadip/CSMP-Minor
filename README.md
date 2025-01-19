fb5dc81876274fd414a462c1f865410815fd857e
soymadip <soumadip377@gmail.com>
2025-01-19 20:19:16 +0530

    delete my source code

--- README.md
+++
@@ -1,5 +0,0 @@
-<div align="center" >
-<img src="src/images/cts-logo.png" width="200">
-<h1>CSMP
-<br>CTS Student Management Portal</h1>
-</div>
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
-// $host = $_SERVER['HTTP_HOST'];
-// $baseUrl = $scheme . '://' . $host . '/csmp/';
-
-$hostUrl = 'http://localhost/csmp';
-
-?>
