0108a20b71a90b4f840273df43fca8d140c49d75
Soumadip Das <soumadip@zohomail.in>
2026-03-15 18:00:16 +0530

    fix: use @if instead of @forelse to fix the ide error message

--- laravel/resources/views/livewire/admin/approve-students.blade.php
+++ laravel/resources/views/livewire/admin/approve-students.blade.php
@@ -15,21 +15,23 @@
             </tr>
         </thead>
         <tbody>
-            @forelse ($pendingStudents as $student)
-                <tr>
-                    <td class="border border-gray-300 dark:border-gray-600 p-2">{{ $student->registration_no }}</td>
-                    <td class="border border-gray-300 dark:border-gray-600 p-2">{{ $student->name }}</td>
-                    <td class="border border-gray-300 dark:border-gray-600 p-2">{{ $student->department }}</td>
-                    <td class="border border-gray-300 dark:border-gray-600 p-2">
-                        <button wire:click="approve('{{ $student->registration_no }}')" class="bg-green-500 text-white p-2 rounded">Approve</button>
-                        <button wire:click="reject('{{ $student->registration_no }}')" class="bg-red-500 text-white p-2 rounded ml-2">Reject</button>
-                    </td>
-                </tr>
-            @empty
+            @if ($pendingStudents->count() > 0)
+                @foreach($pendingStudents as $student)
+                    <tr>
+                        <td class="border border-gray-300 dark:border-gray-600 p-2">{{ $student->registration_no }}</td>
+                        <td class="border border-gray-300 dark:border-gray-600 p-2">{{ $student->name }}</td>
+                        <td class="border border-gray-300 dark:border-gray-600 p-2">{{ $student->department }}</td>
+                        <td class="border border-gray-300 dark:border-gray-600 p-2">
+                            <button wire:click="approve('{{ $student->registration_no }}')" class="bg-green-500 text-white p-2 rounded">Approve</button>
+                            <button wire:click="reject('{{ $student->registration_no }}')" class="bg-red-500 text-white p-2 rounded ml-2">Reject</button>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
                 <tr>
                     <td colspan="4" class="text-center p-4">No pending students.</td>
                 </tr>
-            @endforelse
+            @endif
         </tbody>
     </table>
         <div wire:key="pagination">
